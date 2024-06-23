8c1b60d013bfac7162dfcbda0cbf133159bb6a39
joaozinkrluu <joaopaulocamposl12345@gmail.com>
2024-06-23 17:27:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,6 +25,11 @@ const botRoutes = require('./routes/bots');
 // Use Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/bots', botRoutes);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 // Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
